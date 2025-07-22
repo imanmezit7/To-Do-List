2394a907aaa2cd5a59c87d514f1ad2c42d51e9f4
imanmezit7 <imanmezit7@gmail.com>
2025-07-22 11:24:09 +0200

    layout.tsx

--- src/app/todoiman/layout.tsx
+++ src/app/todoiman/layout.tsx
@@ -1,29 +1,28 @@
-export default function DashboardLayout({
-    children,
-  }: {
-    children: React.ReactNode
-  }) {
-    return (
-      <html lang="en">
-        <body>
-          <header
+import React from 'react'
+
+export default function ToDoLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        <header
           style={{
-            backgroundColor:"white",           
-            textAlign:"center",
-            verticalAlign:"top",
-            color:"darkblue",
-            fontFamily:"trebuchet ms",
-            fontSize:"150%"
-            }}>
-                
-            <b>
-                To-Do List 📝
-            </b>
+            backgroundColor: "white",
+            textAlign: "center",
+            verticalAlign: "top",
+            color: "darkblue",
+            fontFamily: "Trebuchet MS",
+            fontSize: "150%",
+          }}
+        >
+          <b>To-Do List 📝</b>
+        </header>
 
-          </header>
-
-          <main>{children}</main>
-        </body>
-      </html>
-    )
-  }
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}

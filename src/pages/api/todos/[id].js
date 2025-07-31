6f7efd7cebca10b3d0b1b954396202ee418f89e0
imanmezit7 <imanmezit7@gmail.com>
2025-07-31 12:51:07 +0200

    Add files via upload

---
+++ src/pages/api/todos/[id].js
@@ -0,0 +1,36 @@
+import { updateTodo, deleteTodo } from '../../../app/todoiman/todoapi.js';
+
+export default async function handler(req, res) 
+{
+  const { id } = req.query;
+
+  if (req.method === 'PUT') 
+  {
+    try 
+    {
+      const updated = await updateTodo(id, req.body);
+      res.status(200).json(updated);
+    } 
+    catch (err) 
+    {
+      res.status(500).json({ error: 'Failed to update todo' });
+    }
+  } 
+  else if (req.method === 'DELETE') 
+  {
+    try 
+    {
+      await deleteTodo(id);
+      res.status(204).end();
+    } 
+    catch (err) 
+    {
+      res.status(500).json({ error: 'Failed to delete todo' });
+    }
+  } 
+  else 
+  {
+    res.setHeader('Allow', ['PUT', 'DELETE']);
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+}

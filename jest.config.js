e3445754f823b83893f3cb207a0e33136df9a759
imanmezit7 <imanmezit7@gmail.com>
2025-08-14 13:05:07 +0200

    jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,10 @@
-// jest.config.js
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
-  testEnvironment: 'jsdom',
-  transform: {
-    '^.+\\.(t|j)sx?$': 'ts-jest',
-  },
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
-  },
+  testEnvironment: 'jsdom', // for React testing
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-  testMatch: ['**/?(*.)+(test).[jt]s?(x)'],
-  transformIgnorePatterns: ['<rootDir>/node_modules/'],
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest',
+  },
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 };
